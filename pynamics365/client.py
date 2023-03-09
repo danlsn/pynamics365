c18875f2762c4d985c77b77bae9f1104c6131ada
Dan Lawson <52593003+danlsn@users.noreply.github.com>
2023-03-10 10:59:12 +1100

    feat: scaffold dynamics client

---
+++ pynamics365/client.py
@@ -0,0 +1,25 @@
+class DynamicsAuth:
+    token = None
+    ...
+
+
+class DynamicsClient(DynamicsAuth):
+    base_url = None
+    ...
+
+
+class AsyncDynamicsClient:
+    ...
+
+
+class DynamicsRequest:
+    ...
+
+
+class DynamicsEntity:
+    logical_name = None
+    ...
+
+
+class DynamicsQuery:
+    ...
